2597b3aed603df6537b200f03b47e4f9dc005bb5
Steven Fisher <steven.fisher22@gmail.com>
2019-04-11 18:22:13 -0500

    working app - initial view

--- src/flags.js
+++ src/flags.js
@@ -1,7 +1,7 @@
 import gryffindor from './images/gryffindor.png';
 import ravenclaw from './images/ravenclaw.png';
-import hufflepuff from './images/hufflepuff/png';
-import slytherin from './images/slytherin/png';
+import hufflepuff from './images/hufflepuff.png';
+import slytherin from './images/slytherin.png';
 
 export default {
     gryffindor,
--- src/index.js
+++ src/index.js
@@ -6,17 +6,72 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 
+// Components
+import flags from './flags';
+
 // CSS
 import './index.css';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Test
-            </div>
-        )
+// *****************************************************************************************
+
+// Initial State
+const initialState = [
+    {
+        id: 0,
+        name: 'Gryffindor',
+        image: flags.gryffindor,
+        points: 50
+    },
+    {
+        id: 1,
+        name: 'Ravenclaw',
+        image: flags.ravenclaw,
+        points: 100
+    },
+    {
+        id: 2,
+        name: 'Hufflepuff',
+        image: flags.hufflepuff,
+        points: 50
+    },
+    // one is missing
+];
+
+// Reducer
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        default: 
+            return state
     }
 }
 
-ReactDOM. render(<App />, document.querySelector('#root'))
+// Store
+const store = createStore(reducer);
+
+// Main App
+const SchoolAdmin = ({ houses }) => {
+    return (
+        <main>
+            {houses.map(house => (
+                <div key={house.id}>
+                    <img src={house.image} alt={house.name}/>
+                    <div>{house.points} points</div>
+                </div>
+            ))}
+        </main>
+    )
+}
+
+// Map State to Props
+const mapState = state => ({
+    houses: state
+});
+
+const ConnectedApp = connect(mapState)(SchoolAdmin);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedApp />
+    </Provider>, 
+    document.querySelector('#root')
+);
